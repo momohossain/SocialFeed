5a91f66cf7a3fc162157eb34118c07e2a9a254ec
Saad Khan <saad@thefastbite.com>
2018-01-27 17:18:22 -0500

    Integrated the Facebook SDK

--- app/src/main/java/com/example/ikhan/socialfeed/MainActivity.java
+++ app/src/main/java/com/example/ikhan/socialfeed/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.ikhan.socialfeed;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.FaceDetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class MainActivity extends Activity {
+
+    TextView txtStatus;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        intializeControls();
+        loginWithFB();
+
+    }
+    private void intializeControls(){
+        callbackManager=CallbackManager.Factory.create();
+        txtStatus=(TextView)findViewById(R.id.Status);
+        loginButton=(LoginButton)findViewById(R.id.login_button);
+
+    }
+    private void loginWithFB(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtStatus.setText("Login Success\n"+loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                txtStatus.setText("Login Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                txtStatus.setText("Login Error: "+error.getMessage());
+
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
